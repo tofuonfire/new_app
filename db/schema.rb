ac1dad2a4241e5037b83a93582dafdae5c7aa89a
NT <47516714+tofuonfire@users.noreply.github.com>
2019-06-15 11:33:15 +0900

    Make a basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_031346) do
+ActiveRecord::Schema.define(version: 2019_06_14_022514) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  it "is invalid without a name" do
+    @user = FactoryBot.build(:user, name: nil)
+    @user.save
+    expect(@user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a email" do
+    @user = FactoryBot.build(:user, email: nil)
+    @user.save
+    expect(@user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with too long name" do
+    @user = FactoryBot.build(:user, name: "a" * 51)
+    @user.save
+    expect(@user.errors[:name]).to include("is too long (maximum is 50 characters)")
+  end
+
+  it "is invalid with a duplicate email address" do
+    duplicate_user = FactoryBot.create(:user, email: "user@example.com")
+    user = FactoryBot.build(:user, email: duplicate_user.email.upcase)
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "saves email addresses as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    user = FactoryBot.create(:user, email: mixed_case_email)
+    expect(user.reload.email).to eq mixed_case_email.downcase
+  end
+
+  it "is invalid with a blank password" do
+    user = FactoryBot.build(:user, password: " " * 6)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "is invalid with a password less than 6 characters" do
+    user = FactoryBot.build(:user, password: "a" * 5)
+    user.valid?
+    expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+  end
+  
 end
