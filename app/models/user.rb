28477216680121742d689b8937f7bd702d488c1d
NT <47516714+tofuonfire@users.noreply.github.com>
2019-07-03 11:06:20 +0900

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :validatable, authentication_keys: [:login]
+         :rememberable, :validatable, :confirmable, authentication_keys: [:login]
   validates :username, namespace: true, presence: true, uniqueness: true, length: { maximum: 50 }
   attr_writer :login
 
