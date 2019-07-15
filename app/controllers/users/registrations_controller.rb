1bfa2aad999fc1252ea206bb1f8819d45efb796e
NT <47516714+tofuonfire@users.noreply.github.com>
2019-07-15 13:54:39 +0900

    Update profile edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,25 +59,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+    def update_resource(resource, params)
+      return super if params["password"]&.present?
+      resource.update_without_password(params.except("current_password"))
+    end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+    def after_update_path_for(resource)
+      edit_user_registration_path
+    end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_up_params
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    # end
 
-  # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    confirm_email_path
-  end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_account_update_params
+    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    # end
+
+    # The path used after sign up.
+    # def after_sign_up_path_for(resource)
+    #   super(resource)
+    # end
+
+    # The path used after sign up for inactive accounts.
+    def after_inactive_sign_up_path_for(resource)
+      confirm_email_path
+    end
 end
--- app/validators/namespace_validator.rb
+++ app/validators/namespace_validator.rb
@@ -1,7 +1,7 @@
 class NamespaceValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value =~ PathRegex.root_namespace_path_regex
-      record.errors.add(attribute, "has already been taken")
+      record.errors.add(attribute, "は既に使用されています。")
     end
   end
 end
