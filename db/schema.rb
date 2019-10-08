ee23f3f0143adeec94e46cf7eadaf523767f649e
NT <gregoryandthehawk2008@gmail.com>
2019-10-08 20:22:53 +0900

    メールアドレスを更新するときに確認メールを送信しないようにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_013649) do
+ActiveRecord::Schema.define(version: 2019_10_08_112019) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2019_08_28_013649) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
